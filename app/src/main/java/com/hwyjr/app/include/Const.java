0f0c903c1a5d4966b6b9864d303f4a88d550c199
lvni <findoflife@gmail.com>
2016-09-10 15:56:06 +0800

    推送

--- app/src/main/java/com/hwyjr/app/include/Const.java
+++ app/src/main/java/com/hwyjr/app/include/Const.java
@@ -8,7 +8,7 @@ public class Const {
     public static final Boolean DEBUG = false;
     public static final String WEB_PORTAL = "http://app.hong5ye.com/webapp/";
     public static final String ERROR_PAGE = "file:///android_asset/error.html";
-    // public static final String WEB_PORTAL = "http://app.hong5ye.com/test/webapp/";
+    //  public static final String WEB_PORTAL = "http://app.hong5ye.com/test/webapp/";
     public static final String WEB_HOST = "hong5ye.com";
     public static final String APP_ID = "wxbbab02d24a62a8a2";
 
